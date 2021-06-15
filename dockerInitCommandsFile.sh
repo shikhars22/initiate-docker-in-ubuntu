4476ea2f6a95dd83d22c1a8417e5e7040c41c265
shikhar gupta <shikhars22@gmail.com>
2021-06-15 22:49:24 +0530

    Update dockerInitCommandsFile.sh

--- dockerInitCommandsFile.sh
+++ dockerInitCommandsFile.sh
@@ -12,6 +12,10 @@ sudo echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt update -y
+sudo apt upgrade -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo docker run hello-world
 sudo docker ps -a
 sudo docker images
